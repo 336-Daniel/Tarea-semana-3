b87a56af575d0c490763083f12c63949ac59b61a
CarlosCisneros-cmd <imlol711@gmail.com>
2026-06-29 15:45:29 -0500

    creacion del proyecto con su dockerfile

---
+++ svc-rooms/src/test/java/com/uti/svc_rooms/SvcRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uti.svc_rooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
